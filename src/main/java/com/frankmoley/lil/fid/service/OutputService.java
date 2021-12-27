030ebedc73a954cb6159b942ea8b4813dde42e87
nikhilpawar43 <nikhilpawar43@ymail.com>
2021-12-27 13:28:38 +0530

    Implementing component scan

--- src/main/java/com/frankmoley/lil/fid/service/OutputService.java
+++ src/main/java/com/frankmoley/lil/fid/service/OutputService.java
@@ -1,16 +1,22 @@
 package com.frankmoley.lil.fid.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OutputService {
 
-    private final String name;
+    @Value("${app.name}")
+    private String name;
 
     private final GreetingService greetingService;
     private final TimeService timeService;
 
-    public OutputService(GreetingService greetingService, TimeService timeService, String name){
+    @Autowired
+    public OutputService(GreetingService greetingService, TimeService timeService){
         this.greetingService = greetingService;
         this.timeService = timeService;
-        this.name = name;
     }
 
     public void generateOutput(){
