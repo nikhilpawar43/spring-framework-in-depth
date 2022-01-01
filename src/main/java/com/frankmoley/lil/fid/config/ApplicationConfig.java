fcd600f3a551596dc5314c0cb6db1b1cc789a9fe
nikhilpawar43 <nikhilpawar43@ymail.com>
2022-01-01 23:32:20 +0530

    Define AOP pointcut.

--- src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
+++ src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
@@ -2,11 +2,13 @@ package com.frankmoley.lil.fid.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = {"com.frankmoley.lil.fid"})
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
 }
